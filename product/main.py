2338278e80b73cf43e51c0b2f4055ad4e10dc604
Kumar Gaurav <kumargaurav1527@gmail.com>
2024-07-22 10:52:34 +0530

    Raising Exceptions

--- product/main.py
+++ product/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from schemas import Product, ProductResponse
 import models
 from database import engine, sessionLocal
@@ -40,14 +40,16 @@ def get_products(db: Session = Depends(get_db)):
     products = db.query(models.Product).all()
     return products
 
-@app.get("/products/{id}")
+@app.get("/products/{id}", response_model=ProductResponse)
 def get_product(id: int, db: Session = Depends(get_db)):
     product = db.query(models.Product).filter(models.Product.id == id).first()
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
     return product
 
 
 
-@app.post("/products")
+@app.post("/products", status_code=status.HTTP_201_CREATED)
 def create_product(request: Product, db: Session = Depends(get_db)):
     new_product = models.Product(name=request.name, description=request.description, price=request.price)
     db.add(new_product)
